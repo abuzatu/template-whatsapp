a34d8b71b8a22a3a4e789c507d09530c9c693318
Adrian Buzatu <adrian.buzatu@gmail.com>
2023-04-02 15:21:32 +0200

    docker stop also to removes container to be safe

--- bin/dev/docker-stop.sh
+++ bin/dev/docker-stop.sh
@@ -3,8 +3,9 @@
 
 CONTAINER_NAME="${PROJECT_NAME}"
 
-if  docker ps | grep "$CONTAINER_NAME$"; then
+if  docker ps -a | grep "$CONTAINER_NAME$"; then
     docker stop $CONTAINER_NAME
+    docker rm $CONTAINER_NAME
 else
     echo "Container not running."
 fi
