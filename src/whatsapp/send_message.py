c9911438cf27960857e7dddaa6f70eab3f70445e
Adrian Buzatu <adrian.buzatu@gmail.com>
2023-05-21 19:09:47 +0200

    remove unused import

--- src/whatsapp/send_message.py
+++ src/whatsapp/send_message.py
@@ -7,7 +7,6 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
-from selenium.common.exceptions import WebDriverException
 
 from cli.cli_send_message import CLI
 from utils.logger import request_logger
