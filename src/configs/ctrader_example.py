6eab3e225f5895563a036a955149960c3fc95a1f
Adrian Buzatu <adrian.buzatu@gmail.com>
2023-05-29 23:08:58 +0200

    Rename with _ as for python modules

--- src/configs/ctrader_example.py
+++ src/configs/ctrader_example.py
@@ -1,10 +1,10 @@
-"""Module for config for CTrader."""
+"""Module for config for CTrader as example with some demo values."""
 
 # server
 HOST = "78.129.190.32"
 # account
 SENDER_COMP_ID = "demo.icmarkets.8726506"
-PASSWORD = "1234567890"
+PASSWORD = ""
 CURRENCY = "USD"
 # set by us internally
 CLIENT_ID = 1
